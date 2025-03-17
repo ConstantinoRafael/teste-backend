9e6f14fef43c39577adb6be2a990f65b5ea912bc
Rafael <rafael.constantino89@gmail.com>
2025-03-17 06:03:38 -0300

    refact: server to user routes

--- teste-backend/src/index.ts
+++ teste-backend/src/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+
+dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRoutes);
 
 const port = process.env.PORT || 4568;
 
