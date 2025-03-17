c9cf6b72496b37fa8dab6e418032693828eafbc9
Rafael <rafael.constantino89@gmail.com>
2025-03-17 05:15:48 -0300

    feat: user model

---
+++ teste-backend/src/models/User.ts
@@ -0,0 +1,25 @@
+export interface User {
+  id?: number;
+  person_type: EPersonType
+  cnpj?: string;
+  responsibleCpf?: string;   
+  name: string;
+  mobile?: string;
+  phone?: string;
+  email?: string;
+  zip_code?: string;
+  street?: string;
+  number?: string;
+  complement?: string;
+  city?: string;
+  neighborhood?: string;
+  state?: string;
+  termos?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const enum EPersonType {
+  PHYSICS = 'PHYSICS',
+  JURIDIC = 'JURIDIC'
+}
